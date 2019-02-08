fa541d98969f23c74eb575245d3f1e788d9ee7bb
Rafael Teixeira <monoman@gmail.com>
2019-02-08 09:51:03 -0200

    Meetup .NET SP

---
+++ NBitcoin_demo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using NBitcoin;
+
+namespace NBitcoin_demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var privateKey = new Key();
+            PubKey publicKey = privateKey.PubKey;
+            Console.WriteLine($"publicKey: {publicKey}");
+            Console.WriteLine($"publicKey.GetAddress(Network.Main): {publicKey.GetAddress(Network.Main)}"); 
+            Console.WriteLine($"publicKey.GetAddress(Network.TestNet): {publicKey.GetAddress(Network.TestNet)}");
+            var publicKeyHash = publicKey.Hash;
+            Console.WriteLine($"publicKeyHash: {publicKeyHash}"); 
+            var mainNetAddress = publicKeyHash.GetAddress(Network.Main);
+            var testNetAddress = publicKeyHash.GetAddress(Network.TestNet);
+            Console.WriteLine($"mainNetAddress: {mainNetAddress}"); 
+            Console.WriteLine($"testNetAddress: {testNetAddress}");
+            Console.ReadLine();
+        }
+    }
+}
